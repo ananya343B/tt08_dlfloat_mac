85fe138e29edb47008035657f561629b9a84bd02
Nidz13 <166122007+Nidz13@users.noreply.github.com>
2024-08-07 12:04:19 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -170,7 +170,7 @@ module dlfloat_adder(input clk, input [15:0]a1, input [15:0]b1, output reg[15:0]
   	
    
     
-  always@(posedge clk) begin
+	always@(*) begin
         //stage 1
 	     e1_80 = a1[14:9];
 	     e2_80 = b1[14:9];
