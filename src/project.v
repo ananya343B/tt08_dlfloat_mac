06bca6263e671fcd7cae031924524fe3bd6c24e5
Nidz13 <166122007+Nidz13@users.noreply.github.com>
2024-08-07 08:59:14 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,48 +5,79 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_dlfloatmac (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
+wire [15:0]data_in;
+wire [15:0] c;
+wire [15:0]wa,wb;
+wire write_en;
+
+
+assign uio_oe = write_en?8'b11111111:8'b00000000;
+
+
+assign data_in = {uio_in,ui_in};
+
+
+reg_wrapper wrap(clk,rst_n,data_in,wa,wb,write_en);
+dlfloat_mac MAC(clk,rst_n,wa,wb,c);
+
+
+assign uio_out = c[15:8];
+assign uo_out = c[7:0];
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  
 endmodule
 
+module reg_wrapper(
+    input clk,
+    input rst_n,
+    input [15:0] data_in,
+    output reg [15:0] reg_a,
+    output reg [15:0] reg_b,
+    output reg write_en
+);
 
+reg [1:0] state;
+reg [15:0] temp_data;
 
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        state <= 2'b00; 
+        reg_a <= 16'b0;
+        reg_b <= 16'b0;
+        write_en <= 0;
+    end
+    else begin
+        case (state)
+            2'b00: begin
+                temp_data <= data_in;
+                reg_a<=16'b0;
+                reg_b<=16'b0;
+                state <= 2'b01;
+            end
+            2'b01: begin
+                reg_a <= temp_data;
+                reg_b <= data_in;
+                write_en <= 1;
+                state <= 2'b00;
+            end
+            default: state <= 2'b00; 
+        endcase
+    end
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+endmodule
 
 module dlfloat_mac(clk,rst_n,a,b,c_out);
     input [15:0]a,b;
