bfb83e70333a478465c523ac6fd5b4630da0652a
ananya343B <142582353+ananya343B@users.noreply.github.com>
2024-08-18 15:19:50 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -91,11 +91,11 @@ reg [1:0] state;
     else begin
         case (state)
             2'b00: begin
-              c_byte <= c[7:0];
+		    c_byte <= c[15:8];
                 state <= 2'b01;
             end
             2'b01: begin
-              c_byte <= c[15:8]; 
+		    c_byte <= c[7:0]; 
                 state <= 2'b00;
             end
             default: state <= 2'b00; 
